30af3d67ee11f2489238ed574b6c21803d3619c9
Shelby mcgarrah <shelbymcgarrah96@gmail.com>
2020-01-20 16:02:03 -0600

    connect to DB using pymongo driver

--- handler.py
+++ handler.py
@@ -1,10 +1,19 @@
 import json
+import os
+from pymongo import MongoClient
 
 
 def main(event, context):
+    username = os.environ['DB_CREDENTIAL_USERNAME']
+    password = os.environ['DB_CREDENTIAL_PASSWORD']
+    url = "mongodb+srv://" + username + ":" + password + ">@cluster0-gpeio.mongodb.net/test?retryWrites=true&w=majority";
+    client = MongoClient(url);
+    db = client.RPG;
+    results = db.list_entries.find_one({type: 'text'})
     body = {
         "message": "Go Serverless v1.0! Your function executed successfully!",
-        "input": event
+        "input": event, 
+        "dbResult": results
     }
 
     response = {
