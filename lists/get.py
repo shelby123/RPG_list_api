00fc5912132e54eba0bb1675c7c09088dcef20be
dennystasaski <dstasaski@gmail.com>
2020-01-21 20:26:50 -0600

    add mongo functionality to get list

--- lists/get.py
+++ lists/get.py
@@ -1,32 +1,37 @@
 import json
 import os
 from pymongo import MongoClient
+from bson import json_util
+from bson.objectid import ObjectId
+
 
 # TODO: refactor DB connection into separate file. 
 def get(event, context):
-    username = os.environ['DB_CREDENTIAL_USERNAME']
-    password = os.environ['DB_CREDENTIAL_PASSWORD']
-    url = "mongodb+srv://" + username + ":" + password + "@cluster0-gpeio.mongodb.net/test?retryWrites=true&w=majority";
-    client = MongoClient(url);
-    db = client.RPG;
+    collection = get_db_collection()
+    doc = query_list(collection, event['list_id'])
+
     body = {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
+        "message": "get list lambda invoked",
         "input": event, 
-        "idRead": event['pathParameters']['id']
+        "list": json.dumps(doc, sort_keys=True, indent=4, default=json_util.default)
     }
 
     response = {
         "statusCode": 200,
         "body": json.dumps(body)
     }
-
     return response
 
-    # Use this code if you don't use the http event with the LAMBDA-PROXY
-    # integration
-    """
-    return {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
-        "event": event
-    }
-    """
+
+def get_db_collection():
+    username = os.environ['DB_CREDENTIAL_USERNAME']
+    password = os.environ['DB_CREDENTIAL_PASSWORD']
+    url = "mongodb+srv://" + username + ":" + password + "@cluster0-gpeio.mongodb.net/test?retryWrites=true&w=majority"
+    client = MongoClient(url)
+    db = client.RPG
+    return db.list_entries
+
+
+def query_list(collection, list_id):
+    return collection.find_one({'_id': ObjectId(list_id)})
+
